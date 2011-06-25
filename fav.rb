7d4d7adbab44740d045530b0af9e049c3582dcde
MATSUMOTO Katsuyoshi <matsumoto.katsuyoshi+github@gmail.com>
2011-06-26 00:00:51 +0900

    キーワードで自動ふぁぼふぁぼが出来るようになったんです． キーワードもユーザ同様","で区切ってください．

--- fav.rb
+++ fav.rb
@@ -7,26 +7,43 @@ Module.new do
       Plugin.call(:setting_tab_regist, main, 'ふぁぼ')
     end
     plugin.add_event(:update) do |service, message|
-      if UserConfig[:fav_users] && ( UserConfig[:auto_fav] ||
-                                     UserConfig[:auto_rt] )
+      if ( UserConfig[:fav_users] || UserConfig[:fav_keywords] ) &&
+          ( UserConfig[:auto_fav] || UserConfig[:auto_rt] )
         UserConfig[:fav_users].split(',').each do |user|
-          fav( user.strip, message )
+          users( user.strip, message )
+        end
+        UserConfig[:fav_keywords].split(',').each do |key|
+          keywords( key.strip, message )
         end
       end
     end
   end
 
-  def self.fav( target, msg )
-    msg.each do |m|
-      user = m.idname
-      if user == target
-        # ふぁぼるよ
-        unless( m.favorite? || m[:retweet] )
-          m.favorite(true) if UserConfig[:auto_fav]
+  def self.users( target, msg )
+    if !msg.empty?
+      msg.each do |m|
+        user = m.idname
+        if user == target
+          # ふぁぼるよ
+          unless( m.favorite? || m[:retweet] )
+            m.favorite(true) if UserConfig[:auto_fav]
+          end
+          # リツイートするよ
+          unless m[:retweet]
+            m.retweet if UserConfig[:auto_rt]
+          end
         end
-        # リツイートするよ
-        unless m[:retweet]
-          m.retweet if UserConfig[:auto_rt]
+      end
+    end
+  end
+
+  def self.keywords( key, msg )
+    if !msg.empty?
+      msg.each do |m|
+        # p m.to_s
+        if /#{key}/u =~ m.to_s
+          m.favorite(true) if UserConfig[:auto_fav] && !m.favorite?
+          m.retweet if UserConfig[:auto_rt] && !m[:retweet]
         end
       end
     end
@@ -37,7 +54,8 @@ Module.new do
     fav_u = Mtk.group("ふぁぼるよ",
                       Mtk.boolean(:auto_fav, "じどうふぁぼ"),
                       Mtk.boolean(:auto_rt, "じどうりついーと"),
-                      Mtk.input(:fav_users,"ふぁぼるゆーざ"))
+                      Mtk.input(:fav_users,"ふぁぼるゆーざ"),
+                      Mtk.input(:fav_keywords, "きーわーど"))
     # fav_a = Mtk.boolean(:auto_fav, "じどうふぁぼ")
     # fav_r = Mtk.boolean(:auto_rt, "じどうりついーと")
     box.closeup(fav_u)
