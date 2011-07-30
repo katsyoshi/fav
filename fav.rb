8c502a018996171974e1db716f96ad400fc0d5df
MATSUMOTO Katsuyoshi <matsumoto.katsuyoshi+github@gmail.com>
2011-07-30 14:09:35 +0900

    ちえんふぁぼふぁぼが導入されましたよ

--- fav.rb
+++ fav.rb
@@ -10,12 +10,12 @@ Module.new do
       if UserConfig[:auto_fav] || UserConfig[:auto_rt]
         if UserConfig[:fav_users]
           UserConfig[:fav_users].split(',').each do |user|
-            users( user.strip, message )
+            Thread.new { users( user.strip, message ) }
           end
         end
         if UserConfig[:fav_keywords]
           UserConfig[:fav_keywords].split(',').each do |key|
-            keywords( key.strip, message )
+            Thread.new{ keywords( key.strip, message ) }
           end
         end
       end
@@ -27,17 +27,25 @@ Module.new do
       msg.each do |m|
         user = m.idname
         if user == target
+          rt = m[:retweet]
+          fav = m.favorite?
+          delay_time(target) #遅延させるよ
           # ふぁぼるよ
-          unless( m.favorite? || m[:retweet] )
-            m.favorite(true) if UserConfig[:auto_fav]
-          end
+          if !fav && !rt && UserConfig[:auto_fav]
+            m.favorite(true) end
           # リツイートするよ
-          unless m[:retweet]
-            m.retweet if UserConfig[:auto_rt]
-          end
+          m.retweet if !rt && UserConfig[:auto_rt]
         end
       end
     end
+  end
+
+  def self.delay_time(target)
+    sec = 0
+    sec = UserConfig[:fav_lazy].to_i if !UserConfig[:fav_lazy].empty?
+    puts "start->"+target+":#{sec}"+Time.now.to_s
+    sleep(rand(sec).to_i) 
+    puts 'end->'+target+":"+Time.now.to_s
   end
 
   def self.keywords( key, msg )
@@ -45,6 +53,7 @@ Module.new do
       msg.each do |m|
         # p m.to_s
         if /#{key}/u =~ m.to_s
+          delay_time(m.idname) #遅延させるよ
           m.favorite(true) if UserConfig[:auto_fav] && !m.favorite?
           m.retweet if UserConfig[:auto_rt] && !m[:retweet]
         end
@@ -58,9 +67,9 @@ Module.new do
                       Mtk.boolean(:auto_fav, "じどうふぁぼ"),
                       Mtk.boolean(:auto_rt, "じどうりついーと"),
                       Mtk.input(:fav_users,"ふぁぼるゆーざ"),
-                      Mtk.input(:fav_keywords, "きーわーど"))
-    # fav_a = Mtk.boolean(:auto_fav, "じどうふぁぼ")
-    # fav_r = Mtk.boolean(:auto_rt, "じどうりついーと")
+                      Mtk.input(:fav_keywords, "きーわーど"),
+                      # adjustment使いたいがなんか使えない
+                      Mtk.input(:fav_lazy, "ちえん時間"))
     box.closeup(fav_u)
   end
 
