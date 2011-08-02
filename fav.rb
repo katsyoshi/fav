efff74cabef25d1b9aefaaea1c1736a9ab583959
MATSUMOTO Katsuyoshi <matsumoto.katsuyoshi+github@gmail.com>
2011-08-02 20:57:13 +0900

    Thread からerialThreadGroupに変更

--- fav.rb
+++ fav.rb
@@ -1,21 +1,25 @@
 # -*- coding: utf-8 -*-
 # requrie 'date'
+miquire :core, "serialthread"
 Module.new do
   def self.boot
     plugin = Plugin::create(:fav_timeline)
     plugin.add_event(:boot) do |service|
       Plugin.call(:setting_tab_regist, main, 'ふぁぼ')
     end
+    @thread = SerialThreadGroup.new
+    # @thread.max = 10000
     plugin.add_event(:update) do |service, message|
       if UserConfig[:auto_fav] || UserConfig[:auto_rt]
         if UserConfig[:fav_users]
           UserConfig[:fav_users].split(',').each do |user|
-            Thread.new { users( user.strip, message ) }
+            # SerialThreadGroup.new { users( user.strip, message ) }
+            @thread.new { users( user.strip, message ) }
           end
         end
         if UserConfig[:fav_keywords]
           UserConfig[:fav_keywords].split(',').each do |key|
-            Thread.new{ keywords( key.strip, message ) }
+            @thread.new{ keywords( key.strip, message ) }
           end
         end
       end
@@ -29,10 +33,10 @@ Module.new do
         if user == target
           rt = m[:retweet]
           fav = m.favorite?
-          delay_time(target) #遅延させるよ
+          #遅延させるよ
+          delay_time(target) if !fav && !rt
           # ふぁぼるよ
-          if !fav && !rt && UserConfig[:auto_fav]
-            m.favorite(true) end
+          m.favorite(true) if !fav && !rt && UserConfig[:auto_fav]
           # リツイートするよ
           m.retweet if !rt && UserConfig[:auto_rt]
         end
@@ -51,9 +55,8 @@ Module.new do
   def self.keywords( key, msg )
     if !msg.empty?
       msg.each do |m|
-        # p m.to_s
         if /#{key}/u =~ m.to_s
-          delay_time(m.idname) #遅延させるよ
+          delay_time(m.idname) if !m.favorite?
           m.favorite(true) if UserConfig[:auto_fav] && !m.favorite?
           m.retweet if UserConfig[:auto_rt] && !m[:retweet]
         end
@@ -68,7 +71,7 @@ Module.new do
                       Mtk.boolean(:auto_rt, "じどうりついーと"),
                       Mtk.input(:fav_users,"ふぁぼるゆーざ"),
                       Mtk.input(:fav_keywords, "きーわーど"),
-                      # adjustment使いたいがなんか使えない
+                      # Mtk.adjustment使いたいがなんか使えない
                       Mtk.input(:fav_lazy, "ちえん時間"))
     box.closeup(fav_u)
   end
